d6cf3f70052999128fb5dcda513d62ac2c608bb4
Kei-no-suke <55536596+Kei-no-suke@users.noreply.github.com>
2023-07-26 18:02:03 +0900

    add AlarmScheduler

---
+++ app/src/main/java/com/example/simplecomposetimer/alarm/AndroidAlarmScheduler.kt
@@ -0,0 +1,37 @@
+package com.example.simplecomposetimer.alarm
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import java.time.ZoneId
+
+class AndroidAlarmScheduler(
+    private val context: Context
+): AlarmScheduler {
+    private val alarmManager = context.getSystemService(AlarmManager::class.java)
+    override fun schedule(item: AlarmItem) {
+        val intent = Intent(context, AlarmReceiver::class.java)
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            item.time.atZone(ZoneId.systemDefault()).toEpochSecond() * 1000,
+            PendingIntent.getBroadcast(
+                context,
+                item.id,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        )
+    }
+
+    override fun cancel(item: AlarmItem) {
+        alarmManager.cancel(
+            PendingIntent.getBroadcast(
+                context,
+                item.id,
+                Intent(context, AlarmReceiver::class.java),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        )
+    }
+}
