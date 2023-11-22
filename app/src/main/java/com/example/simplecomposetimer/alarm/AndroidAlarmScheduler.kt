5af18631bd174750e89434b046d4289d90c05f63
Kei-no-suke <55536596+Kei-no-suke@users.noreply.github.com>
2023-11-22 16:27:43 +0900

    Added screenshot image

--- app/src/main/java/com/example/simplecomposetimer/alarm/AndroidAlarmScheduler.kt
+++ app/src/main/java/com/example/simplecomposetimer/alarm/AndroidAlarmScheduler.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import java.time.ZoneId
 
 class AndroidAlarmScheduler(
@@ -17,7 +18,7 @@ class AndroidAlarmScheduler(
             item.time.atZone(ZoneId.systemDefault()).toEpochSecond() * 1000,
             PendingIntent.getBroadcast(
                 context,
-                item.id,
+                item.hashCode(),
                 intent,
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
